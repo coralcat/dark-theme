40834c0642534389a55e64bcb73aa915423aebe5
soridive <soridive@naver.com>
2020-11-13 15:40:48 +0900

    Update 2020-11-13T06:40:46.502Z

--- src/assets/script/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const main = document.querySelector('main');
-    const toggleSwitch = document.querySelector('.switch');
-
-    toggleSwitch.addEventListener('click', () => {
-        main.classList.toggle('dark-theme');
-    })
-});
